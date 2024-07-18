8271a0ac48be98d903cd80f8b3bf4a74bff0402c
Chinmay Khanzode <70485580+ChinmayKhanzode@users.noreply.github.com>
2024-07-18 17:06:36 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({ baseURL: `http://localhost:5500/` });
-// const API = axios.create({ baseURL: `https://youtubeclone5031.herokuapp.com/` });
+// const API = axios.create({ baseURL: `https://goldfish-app-gfj9g.ondigitalocean.app/` });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
