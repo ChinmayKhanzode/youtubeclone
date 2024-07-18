e7b46cb0de11fe6aab4b274ac7ed983ed9f05ce9
Chinmay Khanzode <70485580+ChinmayKhanzode@users.noreply.github.com>
2024-07-18 17:47:11 +0530

    hello

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: `http://localhost:5500/` });
-// const API = axios.create({ baseURL: `https://goldfish-app-gfj9g.ondigitalocean.app/` });
+// const API = axios.create({ baseURL: `http://localhost:5500/` });
+const API = axios.create({ baseURL: `https://goldfish-app-gfj9g.ondigitalocean.app/` });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
